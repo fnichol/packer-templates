8ba0fdf6d2c873400e082fe886664e86f6136934
Fletcher Nichol <fnichol@nichol.ca>
2013-08-11 21:15:22 -0600

    Fix support for ubuntu-13.04 in vmware_fusion.
    Thanks to https://github.com/misheska/basebox-packer for the very
    precise patches and strategy.

--- ubuntu-13.04/scripts/vagrant.sh
+++ ubuntu-13.04/scripts/vagrant.sh
@@ -19,7 +19,55 @@ if [ -f /home/vagrant/.vmfusion_version ]; then
     mkdir ${mount_point}
     cd ${home_dir}
     /bin/mount -o loop ${iso_name} ${mount_point}
-    tar zxf ${mount_point}/*.tar.gz && cd vmware-tools-distrib && ./vmware-install.pl --default
+    tar zxf ${mount_point}/*.tar.gz
+
+    # VMwareTools 9.2.2-893683 doesn't work with the 3.8 kernel
+    if [ -f "${mount_point}/VMwareTools-9.2.2-893683.tar.gz" ]; then
+      # Create symlinks so the vmware-install.pl can find header files
+      src="/lib/modules/$(uname -r)/build/include/linux"
+      cd ${src}
+      for f in utsrelease.h autoconf.h uapi/linux/version.h ; do
+        ln -sf ../generated/$f
+      done ; unset f
+
+      # Mount floppy device with patches
+      mkdir -p /mnt/floppy
+      modprobe floppy
+      mount -t vfat /dev/fd0 /mnt/floppy
+
+      cd ${home_dir}/vmware-tools-distrib
+
+      # Patch so vmci successfully compiles
+      cd ${home_dir}/vmware-tools-distrib/lib/modules/source
+      if [ ! -f vmci.tar.orig ]; then
+        cp vmci.tar vmci.tar.orig
+      fi
+      rm -rf vmci-only
+      tar xf vmci.tar
+      cd ${home_dir}/vmware-tools-distrib/lib/modules/source/vmci-only
+      patch -p1 < /mnt/floppy/vmware9.k3.8rc4.patch
+      cd ${home_dir}/vmware-tools-distrib/lib/modules/source
+      tar cf vmci.tar vmci-only
+      rm -rf vmci-only
+
+      # patch so vmhgfs successfully compiles
+      cd ${home_dir}/vmware-tools-distrib/lib/modules/source
+      if [ ! -f vmhgfs.tar.orig ]; then
+        cp vmhgfs.tar vmhgfs.tar.orig
+      fi
+      rm -rf vmhgfs-only
+      tar xf vmhgfs.tar
+      cd ${home_dir}/vmware-tools-distrib/lib/modules/source/vmhgfs-only/shared
+      patch -p1 < /mnt/floppy/vmware9.compat_mm.patch
+      cd ${home_dir}/vmware-tools-distrib/lib/modules/source
+      tar cf vmhgfs.tar vmhgfs-only
+      rm -rf vmhgfs-only
+
+      umount /mnt/floppy
+      rmdir /mnt/floppy
+    fi
+
+    cd ${home_dir}/vmware-tools-distrib && ./vmware-install.pl --default
     /bin/umount ${mount_point}
     /bin/rm -rf ${home_dir}/${iso_name} ${home_dir}/vmware-tools-distrib
     rmdir ${mount_point}
