51d47172faafe86040339c067f608d7fdf61cad0
Fletcher Nichol <fnichol@nichol.ca>
2022-05-30 10:56:20 -0600

    fix(openbsd): re-run syspatch up to 5 times with exit code 2
    In a situation when syspatch processes a set that contains an update to
    the syspatch program, the program may exit with a code of `2`, in which
    case it is suitable to re-run syspatch to continue processing patch
    sets.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- scripts/openbsd/update.sh
+++ scripts/openbsd/update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -eux
 
+rerun_attempts=5
+
 if [ "$(syspatch -c | wc -l)" -gt 0 ]; then
   while true; do
     if pgrep -qxf '/bin/ksh .*reorder_kernel'; then
@@ -7,10 +9,30 @@ if [ "$(syspatch -c | wc -l)" -gt 0 ]; then
       sleep 1
     else
       echo "Running syspatch(8) to apply all outstanding binary patches"
+      set +e
       syspatch
-      sync
-      echo "Rebooting..."
-      reboot
+      ec=$?
+      set -e
+      case $ec in
+        # If exit code == 0 then we are done, break and reboot
+        0)
+          break
+          ;;
+        # If exit code == 2 then retry syspatch a number of times. Note that
+        # exit code of 2 in this case may signal that syspatch was upgraded
+        # when applying a syspatch set
+        2)
+          rerun_attempts=$((rerun_attempts - 1))
+          if [ $rerun_attempts -le 0 ]; then
+            echo "xxx Failed to re-run syspatch multiple times, aborting" 2>&1
+            exit 10
+          fi
+          ;;
+      esac
     fi
   done
 fi
+
+sync
+echo "Rebooting..."
+reboot
